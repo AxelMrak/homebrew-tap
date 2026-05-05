31cc0136459b1169b9984a8b39a15f3e7beee476
Axel Mrak <axelmrak@gmail.com>
2026-05-05 02:03:36 -0300

    add sentry formula

---
+++ Formula/sentry.rb
@@ -0,0 +1,19 @@
+class Sentry < Formula
+  desc "Terminal UI for monitoring OpenCode agent sessions"
+  homepage "https://github.com/AxelMrak/sentry-oc"
+  url "https://github.com/AxelMrak/sentry-oc.git",
+      tag:      "v0.2.0",
+      revision: "main"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    bin.install "sentry"
+  end
+
+  test do
+    assert_match "sentry", shell_output("#{bin}/sentry --help", 1)
+  end
+end
